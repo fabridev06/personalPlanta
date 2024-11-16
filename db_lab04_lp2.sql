17ada416859b21a9078d366df0b9c3aa9cc6e2b5
Fabricio <149898324+fabridev06@users.noreply.github.com>
2024-11-16 15:11:25 -0500

    Actualización de db_lab04_lp2.sql

--- db_lab04_lp2.sql
+++ db_lab04_lp2.sql
@@ -1,27 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
---
--- Host: localhost    Database: bd_lab04_lp2
--- ------------------------------------------------------
--- Server version	8.0.40
+CREATE database bd_lab04_lp2;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE bd_lab04_lp2;
 
---
--- Table structure for table `tb_empleado`
---
-
-DROP TABLE IF EXISTS `tb_empleado`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_empleado` (
   `id_empleado` int NOT NULL AUTO_INCREMENT,
   `ape_mat` varchar(100) NOT NULL,
@@ -32,25 +12,8 @@ CREATE TABLE `tb_empleado` (
   `salario` decimal(38,2) NOT NULL,
   PRIMARY KEY (`id_empleado`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tb_empleado`
---
 
-LOCK TABLES `tb_empleado` WRITE;
-/*!40000 ALTER TABLE `tb_empleado` DISABLE KEYS */;
+
 INSERT INTO `tb_empleado` VALUES (6,'Hernández','Gómez','Calle 123, Ciudad ABC','2020-05-15','Juan',15000.50),(7,'López','Martínez','Avenida 45, Ciudad XYZ','2021-08-20','María',18000.75),(8,'García','Pérez','Callejón 12, Ciudad DEF','2019-02-10','Luis',20000.00),(9,'Ramírez','Rodríguez','Boulevard 78, Ciudad UVW','2023-01-05','Ana',12000.25);
-/*!40000 ALTER TABLE `tb_empleado` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-11-16 15:08:09
